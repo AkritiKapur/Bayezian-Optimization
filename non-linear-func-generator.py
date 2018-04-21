3c19178fafbed2e486127012d209db28a3b38847
Akriti Kapur <akritikapur1@gmail.com>
2018-04-21 16:39:18 -0600

    skeleton for non-linear function added

---
+++ non-linear-func-generator.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generate_non_linear_function_output(input):
+    """
+        Generates f(x)/ output for the input vector
+        according to the non linear function specified.
+    :return: f(x), output vector
+    """
+    pass
+
+
+def plot_function(input, output):
+    """
+        Generates input-output plot for the 2D function
+    :param input:
+    :param output:
+    :return:
+    """
+    pass
+
+if __name__ == '__main__':
+    input_vector = []
+    output_vector = generate_non_linear_function_output(input_vector)
+    plot_function(input_vector, output_vector)
